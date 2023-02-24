954d74fae19ca7b787b8dee0cc11bebc1edcd7c1
Josh Kyuma <103255095+Joshua-Kyuma@users.noreply.github.com>
2023-02-24 19:38:14 +0300

    Update app.js
    password

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-joshua:Jose4860@cluster0.73cfw3j.mongodb.net/todolistDB", {useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-joshua:********@cluster0.73cfw3j.mongodb.net/todolistDB", {useNewUrlParser:true});
 
 const itemsSchema = {
   name: String
